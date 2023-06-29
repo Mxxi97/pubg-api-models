afa0a8d2220db61f1165f2b4567706cbf7268d7f
Max Müller <max97m2@gmail.com>
2023-06-29 12:24:43 +0200

    match enums

---
+++ src/models/match/match.enum.ts
@@ -0,0 +1,69 @@
+enum MatchType {
+  AIROYALE = 'airoyale',
+  ARCADE = 'arcade',
+  CUSTOM = 'custom',
+  EVENT = 'event',
+  OFFICIAL = 'official',
+  SEASONAL = 'seasonal',
+  TRAINING = 'training',
+}
+
+enum GameMode {
+  DUO = 'duo',
+  DUO_FPP = 'duo-fpp',
+  SOLO = 'solo',
+  SOLO_FPP = 'solo-fpp',
+  SQUAD = 'squad',
+  SQUAD_FPP = 'squad-fpp',
+  CONQUEST_DUO = 'conquest-duo',
+  CONQUEST_DUO_FPP = 'conquest-duo-fpp',
+  CONQUEST_SOLO = 'conquest-solo',
+  CONQUEST_SOLO_FPP = 'conquest-solo-fpp',
+  CONQUEST_SQUAD = 'conquest-squad',
+  CONQUEST_SQUAD_FPP = 'conquest-squad-fpp',
+  ESPORTS_DUO = 'esports-duo',
+  ESPORTS_DUO_FPP = 'esports-duo-fpp',
+  ESPORTS_SOLO = 'esports-solo',
+  ESPORTS_SOLO_FPP = 'esports-solo-fpp',
+  ESPORTS_SQUAD = 'esports-squad',
+  ESPORTS_SQUAD_FPP = 'esports-squad-fpp',
+  LAB_TPP = 'lab-tpp',
+  LAB_FPP = 'lab-fpp',
+  NORMAL_DUO = 'normal-duo',
+  NORMAL_DUO_FPP = 'normal-duo-fpp',
+  NORMAL_SOLO = 'normal-solo',
+  NORMAL_SOLO_FPP = 'normal-solo-fpp',
+  NORMAL_SQUAD = 'normal-squad',
+  NORMAL_SQUAD_FPP = 'normal-squad-fpp',
+  TDM = 'tdm',
+  WAR_DUO = 'war-duo',
+  WAR_DUO_FPP = 'war-duo-fpp',
+  WAR_SOLO = 'war-solo',
+  WAR_SOLO_FPP = 'war-solo-fpp',
+  WAR_SQUAD = 'war-squad',
+  WAR_SQUAD_FPP = 'war-squad-fpp',
+  ZOMBIE_DUO = 'zombie-duo',
+  ZOMBIE_DUO_FPP = 'zombie-duo-fpp',
+  ZOMBIE_SOLO = 'zombie-solo',
+  ZOMBIE_SOLO_FPP = 'zombie-solo-fpp',
+  ZOMBIE_SQUAD = 'zombie-squad',
+  ZOMBIE_SQUAD_FPP = 'zombie-squad-fpp',
+}
+
+enum MapName {
+  BALTIC_MAIN = 'Baltic_Main',
+  DESERT_MAIN = 'Desert_Main',
+  DIHOROTOK_MAIN = 'DihorOtok_Main',
+  ERANGEL_MAIN = 'Erangel_Main',
+  RANGE_MAIN = 'Range_Main',
+  SAVAGE_MAIN = 'Savage_Main',
+  SUMMERLAND_MAIN = 'Summerland_Main',
+}
+
+enum SeasonState {
+  CLOSED = 'closed',
+  PREPARE = 'prepare',
+  PROGRESS = 'progress',
+}
+
+export { MatchType, GameMode, MapName, SeasonState };
